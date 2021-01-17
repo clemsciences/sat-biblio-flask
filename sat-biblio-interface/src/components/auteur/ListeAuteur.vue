e0482c43896ae039d62c923580a2b7c31c2f837d
Clément Besnier <clemsciences@aol.com>
2021-01-17 21:38:01 +0100

    Added filters on author list

--- sat-biblio-interface/src/components/auteur/ListeAuteur.vue
+++ sat-biblio-interface/src/components/auteur/ListeAuteur.vue
@@ -2,18 +2,27 @@
   <div>
     <h2>Liste des auteurs</h2>
       <p>Double-cliquez sur la ligne pour voir les détails.</p>
-    <b-form-group label="Filtre prénom">
-      <b-input type="search" v-model="firstNameFiltre"/>
-    </b-form-group>
-    <b-form-group label="Filtre nom de famille">
-      <b-input type="search" v-model="familyNameFiltre"/>
-    </b-form-group>
+    <b-row class="my-1">
+      <b-col lg="4">
+        <b-form-group label="Prénom" label-cols-sm="3"
+          label-align-sm="right" label-size="sm" class="mb-0">
+          <b-input type="search" v-model="firstNameFiltre"
+                   placeholder="Filtrer en fonction du prénom"/>
+        </b-form-group>
+      </b-col>
+      <b-col lg="4">
+        <b-form-group label="Nom de famille" label-cols-sm="3"
+          label-align-sm="right" label-size="sm" class="mb-0">
+          <b-input type="search" v-model="familyNameFiltre"
+                   placeholder="Filtrer en fonction du nom de famille"/>
+        </b-form-group>
+      </b-col>
+    </b-row>
     <b-pagination
       v-model="currentPage"
       :total-rows="authorTotalNumber"
       :per-page="perPage"
-      aria-controls="my-table"
-    ></b-pagination>
+      aria-controls="my-table"/>
     <b-table striped bordered hover :items="retrieveAuthors" :fields="fields"
              primary-key="id" :per-page="perPage" :current-page="currentPage"
              :sort-by="sortBy" @row-dblclicked="goToAuthor" :filter="onFilter">
@@ -72,6 +81,7 @@ export default {
       axios.get(url).then(
           (response) => {
             if(response.data.success) {
+              this.currentPage = 1;
               this.authors = response.data.authors;
               callback(this.authors);
             }
