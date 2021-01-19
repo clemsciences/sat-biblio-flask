1f657301726daba24c7a26ee3c408a2da4688665
Clément Besnier <clemsciences@aol.com>
2021-01-19 22:47:09 +0100

    Added filter on reference list

--- sat-biblio-interface/src/components/reference_livre/ListeReferenceLivre.vue
+++ sat-biblio-interface/src/components/reference_livre/ListeReferenceLivre.vue
@@ -2,15 +2,20 @@
   <div>
     <h2>Liste des références</h2>
     <p>Double-cliquez sur la ligne pour voir les détails.</p>
-    <b-form-group label="Filtrer par le titre">
-      <b-input v-model="titreFiltre"/>
-    </b-form-group>
+    <b-row class="my-3">
+      <b-col lg="4">
+        <b-form-group label="Titre" label-cols-sm="3"
+          label-align-sm="right" label-size="sm" class="mb-0">
+          <b-input v-model="titreFiltre" placeholder="Filtrer en fonction du titre"/>
+        </b-form-group>
+      </b-col>
+    </b-row>
     <b-pagination
       v-model="currentPage"
       :total-rows="refTotalNumber"
       :per-page="perPage"
       aria-controls="my-table"
-    ></b-pagination>
+      class="my-3"/>
     <b-table striped bordered hover :items="retrieveRef" :fields="fields"
              primary-key="id" :per-page="perPage" :current-page="currentPage"
              :sort-by="sortBy" @row-dblclicked="goToReference" :filter="onFilter">
@@ -78,6 +83,7 @@ export default {
       axios.get("/api/reference-livre/liste"+params+filterParams).then(
           (response) => {
             if(response.data.success) {
+              this.currentPage = 1;
               this.references = response.data.references;
               callback(this.references);
             }
