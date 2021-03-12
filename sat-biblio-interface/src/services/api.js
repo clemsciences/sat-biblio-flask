cad255c945da0d5087abe70a3d9a81a96d2f9022
Clément Besnier <clemsciences@aol.com>
2021-03-12 18:52:02 +0100

    Fixed checkUserLogin

--- sat-biblio-interface/src/services/api.js
+++ sat-biblio-interface/src/services/api.js
@@ -22,7 +22,7 @@ export function recreatePassword(password) {
 }
 
 export function checkUserLogin() {
-  return axios.post('/user/check_login');
+  return axios.post('/users/check_login');
 }
 
 // endregion
