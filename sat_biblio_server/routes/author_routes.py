d9e2f5b6b59d4dd373d7b728a99b4964247d5062
Clément Besnier <clemsciences@aol.com>
2021-03-16 18:57:03 +0100

    Updated author_routes.py: routes, logs, methods

--- sat_biblio_server/routes/author_routes.py
+++ sat_biblio_server/routes/author_routes.py
@@ -10,6 +10,7 @@ import logging
 
 from sqlalchemy import or_
 
+from data import validation
 from sat_biblio_server.data.models import Author
 from sat_biblio_server.database import db, AuthorDB
 from sat_biblio_server import sat_biblio
@@ -23,9 +24,9 @@ __author__ = ["Clément Besnier <clem@clementbesnier.fr>", ]
 # region auteur
 
 # @sat_biblio.route("/api/auteur/creer", methods=["POST"])
-@sat_biblio.route("/auteurs/", methods=["POST", "GET"])
-@validation_connexion_et_retour_defaut("email", redirect("/api"))
-def auteurs():
+@sat_biblio.route("/authors", methods=["POST", "GET"])
+@validation_connexion_et_retour_defaut("email", redirect("/api"), ["POST"])
+def author():
     """
     POST: add an author
     GET: get list of authors
@@ -38,38 +39,41 @@ def auteurs():
 
         the_query = AuthorDB.query
         # sort_desc = request.args.get("sortDesc")
-        # region filtre
+        # region filter
         first_name = request.args.get("first_name", "")
-        family_name = request.args.get("family_name", "")
         if first_name:
             the_query = the_query.filter(AuthorDB.first_name.like(f"%{first_name}%"))
+            logging.error(f"{first_name}")
+        family_name = request.args.get("family_name", "")
         if family_name:
             the_query = the_query.filter(AuthorDB.family_name.like(f"%{family_name}%"))
+            logging.error(f"{family_name}")
         # endregion
         authors = [{"first_name": author.first_name,
                     "family_name": author.family_name,
                     "id": author.id}
                    for author in the_query.order_by(sort_by).paginate(page=n_page, per_page=size).items]
-        logging.debug(len(authors))
-        return json_result(True, authors=authors)
+        logging.error(len(authors))
+        return json_result(True, authors=authors), 200
     elif request.method == "POST":
         data = request.get_json()
+
         if dv.check_author(data):
-            author = AuthorDB(first_name=data["first_name"],
-                              family_name=data["family_name"])
-            db.session.add(author)
+            author_db = Author.from_data_to_db(data)
+            db.session.add(author_db)
             db.session.commit()
-            return json_result(True)
+            return json_result(True), 201
         else:
-            return jsonify(False)
+            return jsonify(False), 304
 
 
-@sat_biblio.route("/api/auteurs/<int:id_>", methods=["GET", "PUT", "DELETE"])
-@validation_connexion_et_retour_defaut("email", redirect("/"))
-def lire_auteur(id_):
+@sat_biblio.route("/authors/<int:id_>", methods=["GET", "PUT", "DELETE"])
+@validation_connexion_et_retour_defaut("email", redirect("/api"), ["DELETE", "PUT"])
+def authors(id_):
     """
     GET: read an author
     PUT: update an author
+    DELETE: delete an author
 
     :param id_:
     :return:
@@ -78,14 +82,17 @@ def lire_auteur(id_):
     if request.method == "GET":
         author_db = AuthorDB.query.filter_by(id=id_).first()
         author = Author.from_db_to_data(author_db)
-        return json_result(True, author=author)
-    elif request.method == "POST":
+        return json_result(True, author=author), 200
+    elif request.method == "PUT":
         data = request.get_json()
         author = AuthorDB.query.filter_by(id=id_).first()
-        author.first_name = data["first_name"]
-        author.family_name = data["family_name"]
-        db.session.commit()
-        return json_result(True)
+        if validation.check_author(data):
+            author.first_name = data["first_name"]
+            author.family_name = data["family_name"]
+            db.session.commit()
+            return json_result(True), 200
+        else:
+            return json_result(False), 400
     elif request.method == "DELETE":
         id_author = id_
         author_db = AuthorDB.query.filter_by(id=id_author).first()
@@ -94,10 +101,29 @@ def lire_auteur(id_):
         if author_db:
             db.session.delete(author_db)
             db.session.commit()
-        return jsonify({"success": True, })
+        return jsonify({"success": True, }), 204
 
 
-@sat_biblio.route("/api/auteur/chercher-proches", methods=["GET", "POST"])
+@sat_biblio.route("/authors/count", methods=["GET"])
+def authors_count():
+    """
+
+    :return:
+    """
+    the_query = AuthorDB.query
+    # region filtre
+    if "first_name" in request.args:
+        the_query = the_query.filter(AuthorDB.first_name.like(f"%{request.args.get('first_name')}%"))
+
+    if "family_name" in request.args:
+        the_query = the_query.filter(AuthorDB.family_name.like(f"%{request.args.get('family_name')}%"))
+    # endregion
+    number = the_query.count()
+    logging.debug(number)
+    return json_result(True, total=number), 200
+
+
+@sat_biblio.route("/authors/chercher-proches", methods=["GET", "POST"])
 def chercher_auteurs_plus_proches():
     query_result = request.args.get("auteur")
 
@@ -108,10 +134,10 @@ def chercher_auteurs_plus_proches():
 
     res = [{"text": f"{author_db.first_name} {author_db.family_name}", "value": author_db.id}
            for author_db in authors_db]
-    return json_result(True, suggestedAuthors=res)
+    return json_result(True, suggestedAuthors=res), 200
 
 
-@sat_biblio.route("/api/auteur/chercher", methods=["POST"])
+@sat_biblio.route("/authors/chercher", methods=["POST"])
 def chercher_auteurs():
     data = request.get_json()
     the_query = AuthorDB.query
@@ -127,27 +153,7 @@ def chercher_auteurs():
     else:
         results = []
     results = [Author.from_db_to_data(author_db) for author_db in results]
-    return json_result(True, results=results)
+    return json_result(True, results=results), 200
 
 
-# @sat_biblio.route("/api/auteur/liste", methods=["GET"])
-# def listes_auteurs():
-#     pass
-
-
-@sat_biblio.route("/api/auteur/nombre", methods=["GET"])
-def get_author_total_number():
-    the_query = AuthorDB.query
-    # region filtre
-    first_name = request.args.get("first_name", "")
-    family_name = request.args.get("family_name", "")
-
-    if first_name:
-        the_query = the_query.filter(AuthorDB.first_name.like(f"%{first_name}%"))
-    if family_name:
-        the_query = the_query.filter(AuthorDB.family_name.like(f"%{family_name}%"))
-    # endregion
-    number = the_query.count()
-    logging.debug(number)
-    return json_result(True, number=number)
 # endregion
