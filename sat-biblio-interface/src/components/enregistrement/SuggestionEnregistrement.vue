e033597afbfb5043d99072965f0ed00e995e2dc3
Clément Besnier <clemsciences@aol.com>
2021-03-26 00:08:15 +0100

    New component SuggestionEnregistrement.vue

---
+++ sat-biblio-interface/src/components/enregistrement/SuggestionEnregistrement.vue
@@ -0,0 +1,58 @@
+<template>
+  <b-form-group label="Enregistrement">
+    <vue-typeahead-bootstrap
+      v-model="recordQuery"
+      :data="suggestedRecords"
+      :serializer="s => s.text"
+      placeholder="Tapez la cote ou le titre de l'enregistrement"
+      @hit="addRecord($event)"
+    />
+    <b-form-input v-model="value.text" readonly/>
+  </b-form-group>
+
+</template>
+
+<script>
+import {searchNearBookRecords} from "../../services/api";
+
+export default {
+  name: "SuggestionEnregistrement",
+  props: {
+    value: Object // selectedReference
+  },
+  data: function () {
+    return {
+      recordQuery: "",
+      // reference: {value: -1, text: ""},
+      // selectedReference: {text: "", value: -1},
+      suggestedRecords: [],
+    }
+  },
+  methods: {
+    addRecord: function (event) {
+      this.recordQuery = "";
+      this.$emit('input', event);
+    },
+    getSuggestedRecords: function (query) {
+      if(query.length >= 2) {
+        searchNearBookRecords(`record=${query}`)
+            .then((response) => {
+              if (response.data.success) {
+                console.log("suggestedRecords", response.data)
+                this.suggestedRecords = response.data.suggestedRecords;
+              }
+            }).catch();
+      }
+    },
+  },
+  watch: {
+    recordQuery: function (newValue) {
+      this.getSuggestedRecords(newValue);
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
