c114236c77831bb5d9dcf3ed34ec871fdba079c5
Clément Besnier <clemsciences@aol.com>
2021-04-02 23:20:46 +0200

    Added a custom Title with Info bubble

---
+++ sat-biblio-interface/src/components/visuel/Title.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-row align-h="between" class="my-2">
+    <b-col cols="8">
+      <h2><slot>{{ title }}</slot></h2>
+    </b-col>
+    <b-col cols="2" v-if="info.length > 0">
+      <InfoButton :label="info" :id="id"/>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+import InfoButton from "./InfoButton";
+export default {
+  name: "Title",
+  components: {InfoButton},
+  props: {
+    title: String,
+    info: String,
+    id: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
