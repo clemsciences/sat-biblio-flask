84ef328d98395ede7a9c1aad5897854f00db8a78
Clément Besnier <clemsciences@aol.com>
2021-05-24 17:57:43 +0200

    inven.csv is optional

--- sat_biblio_server/routes/import_routes.py
+++ sat_biblio_server/routes/import_routes.py
@@ -9,21 +9,22 @@ import sat_biblio_server.data.import_csv_utils as icu
 
 SCRIPT_PATH = os.path.join(PACKDIR, "scripts", "inven.csv")
 rows = []
-i = 0
-with codecs.open(SCRIPT_PATH, "r", encoding="utf-8") as f:
-    for row in csv.reader(f, delimiter=";"):
-        if i > 0:
-            description = row[0]
-            rows.append({"description": description,
-                         "cote": row[1],
-                         "nb_supp": row[2],
-                         "annee": row[3],
-                         "provenance": row[4],
-                         "theme1": row[5],
-                         "theme2": row[6],
-                         "theme3": row[7],
-                         "index": i})
-        i += 1
+if os.path.exists(SCRIPT_PATH):
+    i = 0
+    with codecs.open(SCRIPT_PATH, "r", encoding="utf-8") as f:
+        for row in csv.reader(f, delimiter=";"):
+            if i > 0:
+                description = row[0]
+                rows.append({"description": description,
+                             "cote": row[1],
+                             "nb_supp": row[2],
+                             "annee": row[3],
+                             "provenance": row[4],
+                             "theme1": row[5],
+                             "theme2": row[6],
+                             "theme3": row[7],
+                             "index": i})
+            i += 1
 
 
 @sat_biblio.route("/import-csv/")
