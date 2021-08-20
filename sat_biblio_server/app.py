c38f52dcb4c0512ec9fa80b8851dbf5faba91b0f
Clément Besnier <clemsciences@aol.com>
2021-08-20 23:58:18 +0200

    Host fixed

--- sat_biblio_server/app.py
+++ sat_biblio_server/app.py
@@ -33,4 +33,4 @@ with app.app_context():
 if __name__ == '__main__':
 
     # app.register_blueprint(sat_biblio, url_prefix="/api")
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, port=5000)
