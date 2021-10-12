86a0e9c54b46043dcd0b9216a37ec504b9292660
Clément Besnier <clemsciences@aol.com>
2021-10-12 23:28:40 +0200

    Added list of authors not approved

---
+++ sat-biblio-interface/src/components/admin/ListeAuteursNonApprouves.vue
@@ -0,0 +1,40 @@
+<template>
+  <ListeEntreesNonApprouvees
+  :retrieve-list-request="retrieveNotValidatedAuthors"
+      :go-to="goTo"
+      caption="Auteur"
+      :per-page="perPage"
+      :get-total-number-request="getNotValidatedAuthorsCount"
+      entry-type="auteur"
+  />
+</template>
+
+<script>
+import {
+  getNotValidatedAuthorsCount,
+  retrieveNotValidatedAuthors,
+} from "@/services/api";
+import ListeEntreesNonApprouvees from "@/components/admin/ListeEntreesNonApprouvees";
+export default {
+  name: "ListeAuteursNonApprouves",
+  components: {ListeEntreesNonApprouvees},
+  methods: {
+    retrieveNotValidatedAuthors: retrieveNotValidatedAuthors,
+    getNotValidatedAuthorsCount: getNotValidatedAuthorsCount,
+    goTo() {
+
+    },
+
+  },
+  data: function () {
+    return {
+      perPage: 20,
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
