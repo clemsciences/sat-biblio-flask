a10755a0fc0996e7077dce9839bb6865d6db448a
Clément Besnier <clemsciences@aol.com>
2021-10-13 23:28:59 +0200

    Added list of record not approved

---
+++ sat-biblio-interface/src/components/admin/ListeEnregistrementsNonApprouves.vue
@@ -0,0 +1,37 @@
+<template>
+  <ListeEntreesNonApprouvees
+      :retrieve-list-request="retrieveNotValidatedBookRecords"
+      :go-to="goTo"
+      caption="Enregistrements non validés"
+      :per-page="perPage"
+      :get-total-number-request="getNotValidatedBookRecordsCount"
+      entry-type="enregistrement"
+  />
+</template>
+
+<script>
+import {getNotValidatedBookRecordsCount, retrieveNotValidatedBookRecords} from "@/services/api";
+import ListeEntreesNonApprouvees from "@/components/admin/ListeEntreesNonApprouvees";
+
+export default {
+  name: "ListeEnregistrementsNonApprouves",
+  components: {ListeEntreesNonApprouvees},
+  data: function() {
+    return {
+      perPage: 20,
+    }
+  },
+  methods: {
+    retrieveNotValidatedBookRecords: retrieveNotValidatedBookRecords,
+    getNotValidatedBookRecordsCount: getNotValidatedBookRecordsCount,
+    goTo() {
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
