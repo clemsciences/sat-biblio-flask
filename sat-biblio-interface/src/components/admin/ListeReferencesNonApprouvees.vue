19b01029b6fbc3a2f4c67b250d791bece6d26c40
Clément Besnier <clemsciences@aol.com>
2021-10-14 23:29:21 +0200

    Added list of references not approved

---
+++ sat-biblio-interface/src/components/admin/ListeReferencesNonApprouvees.vue
@@ -0,0 +1,35 @@
+<template>
+  <ListeEntreesNonApprouvees
+      :go-to="goTo"
+      :retrieve-list-request="retrieveNotValidatedBookReferences"
+      :get-total-number-request="getNotValidatedBookReferencesCount"
+      :per-page="perPage"
+      entry-type="référence bibliographique"
+    />
+
+</template>
+
+<script>
+import ListeEntreesNonApprouvees from "@/components/admin/ListeEntreesNonApprouvees";
+import {getNotValidatedBookReferencesCount, retrieveNotValidatedBookReferences} from "@/services/api";
+export default {
+  name: "ListeReferencesNonApprouvees",
+  components: {ListeEntreesNonApprouvees},
+  data: function() {
+    return {
+      perPage: 20,
+    }
+  },
+  methods: {
+    retrieveNotValidatedBookReferences: retrieveNotValidatedBookReferences,
+    getNotValidatedBookReferencesCount: getNotValidatedBookReferencesCount,
+    goTo() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
