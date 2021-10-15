04e50c605b98e96511526b5bcab5d4f8e3bc3ebe
Clément Besnier <clemsciences@aol.com>
2021-10-15 23:30:11 +0200

    Added list of authors, references and records not approved into manager view

--- sat-biblio-interface/src/components/admin/Gestionnaire.vue
+++ sat-biblio-interface/src/components/admin/Gestionnaire.vue
@@ -4,10 +4,40 @@
            id="id-gestionnaire">
       Page du gestionnaire
     </Title>
-    <b-button v-b-toggle.collapse-users>Voir les utilisateurs</b-button>
-    <b-collapse id="collapse-users">
-      <liste-utilisateur/>
-    </b-collapse>
+    <div class="accordion" role="tablist">
+      <b-row>
+        <b-button v-b-toggle.collapse-users>
+          Voir les utilisateurs
+        </b-button>
+      </b-row>
+      <b-row class="my-2">
+        <b-row role="tab">
+          <b-collapse id="collapse-users"
+                      accordion="manager-accordion"
+                      role="tabpanel">
+            <liste-utilisateur/>
+          </b-collapse>
+        </b-row>
+      </b-row>
+      <b-row>
+        <b-button v-b-toggle.collapse-non-approved-entries >
+              Voir les entrées non approuvées
+        </b-button>
+      </b-row>
+
+      <b-row class="my-2">
+        <b-row role="tab">
+          <b-collapse id="collapse-non-approved-entries"
+                      accordion="manager-accordion"
+                      role="tabpanel">
+            <liste-auteurs-non-approuves/>
+            <liste-references-non-approuvees/>
+            <liste-enregistrements-non-approuves/>
+        </b-collapse>
+        </b-row>
+
+      </b-row>
+    </div>
 
   </b-container>
 </template>
@@ -15,9 +45,15 @@
 <script>
 import Title from "../visuel/Title";
 import ListeUtilisateur from "@/components/admin/ListeUtilisateur";
+import ListeAuteursNonApprouves from "@/components/admin/ListeAuteursNonApprouves";
+import ListeReferencesNonApprouvees from "@/components/admin/ListeReferencesNonApprouvees";
+import ListeEnregistrementsNonApprouves from "@/components/admin/ListeEnregistrementsNonApprouves";
 export default {
 name: "Gestionnaire",
-  components: {Title, ListeUtilisateur},
+  components: {
+    ListeEnregistrementsNonApprouves,
+    ListeReferencesNonApprouvees,
+    ListeAuteursNonApprouves, Title, ListeUtilisateur},
   data: function () {
     return {
 
