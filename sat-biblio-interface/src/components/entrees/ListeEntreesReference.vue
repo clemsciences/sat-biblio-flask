9c96b1ca8d4a8de1c35a806f6d601fe9ccf55b2e
Clément Besnier <clemsciences@aol.com>
2021-10-17 23:34:59 +0200

    See a list of entries associated to a reference

---
+++ sat-biblio-interface/src/components/entrees/ListeEntreesReference.vue
@@ -0,0 +1,45 @@
+<template>
+  <ListeEntrees :retrieve-list-request="getEntryListAssociatedToReference"
+                :go-to="goTo"
+                caption="Enregistrement"
+                :per-page="perPage"
+                :get-total-number-request="getEntryListAssociatedToReferenceCount"
+                :entry-id="referenceId"
+  />
+</template>
+
+<script>
+import ListeEntrees from "@/components/entrees/ListeEntrees";
+import {
+  getEntryListAssociatedToReference,
+  getEntryListAssociatedToReferenceCount
+} from "@/services/api";
+
+export default {
+  name: "ListeEntreesReference",
+  components: {ListeEntrees},
+  props: {
+    referenceId: {
+      type: Number,
+    }
+  },
+  data: function() {
+    return {
+      entryCount: 0,
+      entries: [],
+      perPage: 20,
+    }
+  },
+  methods: {
+    getEntryListAssociatedToReference: getEntryListAssociatedToReference,
+    getEntryListAssociatedToReferenceCount: getEntryListAssociatedToReferenceCount,
+    goTo(item) {
+      console.log(item);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
