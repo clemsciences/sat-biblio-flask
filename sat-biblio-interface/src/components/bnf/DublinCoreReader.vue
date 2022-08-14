59bf64bd3824136cdef29394fe6617b0f3f1d204
Clément Besnier <clemsciences@aol.com>
2022-08-14 22:24:10 +0200

    Added a reader of a Dublin Core entry

---
+++ sat-biblio-interface/src/components/bnf/DublinCoreReader.vue
@@ -0,0 +1,37 @@
+<template>
+  <b-container>
+    <h3>{{ record.title }}</h3>
+    <b-list-group>
+      <b-list-group-item>{{ record.description }}</b-list-group-item>
+      <b-list-group-item>{{ record.creator }}</b-list-group-item>
+      <b-list-group-item>{{ record.publisher }}</b-list-group-item>
+      <b-list-group-item>{{ record.date }}</b-list-group-item>
+      <b-list-group-item>{{ record.subject }}</b-list-group-item>
+      <b-list-group-item>{{ record.type }}</b-list-group-item>
+      <b-list-group-item>{{ record.format }}</b-list-group-item>
+      <b-list-group-item>{{ record.source }}</b-list-group-item>
+      <b-list-group-item>{{ record.relation }}</b-list-group-item>
+      <b-list-group-item>{{ record.rights }}</b-list-group-item>
+
+    </b-list-group>
+
+  </b-container>
+
+</template>
+
+<script>
+export default {
+  name: "DublinCore",
+  props: {
+    record: Object
+  },
+  data: function() {
+    return {
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
