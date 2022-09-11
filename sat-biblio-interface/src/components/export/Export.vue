4fcc9925d0e630a721311a1d814e36136047f284
Clément Besnier <clemsciences@aol.com>
2022-09-11 19:49:15 +0200

    Improved the layout of export view

--- sat-biblio-interface/src/components/export/Export.vue
+++ sat-biblio-interface/src/components/export/Export.vue
@@ -1,12 +1,34 @@
 <template>
   <b-container>
     <h2>Export</h2>
-    <b-button class="mx-3" @click="exporterPDF">
-      Exporter au format PDF
-    </b-button>
-    <b-button class="mx-3" @click="exporterExcel">
-      Exporter au format Excel (.xsl)
-    </b-button>
+    <b-row class="m-3">
+<!--      <b-button class="mx-3" @click="exporterPDF" :disabled="downloading">-->
+<!--        Exporter au format PDF-->
+<!--      </b-button>-->
+      <b-button class="m-2" @click="exporterExcel" :disabled="generating">
+        Générer un export au format Excel (.xlsx)
+      </b-button>
+
+      <b-button class="m-2" @click="exporterCSV" :disabled="generating">
+        Générer un export au format CSV (.csv)
+      </b-button>
+      <b-checkbox v-model="withWeed">Ajouter les ouvrages désherbés</b-checkbox>
+      <b-checkbox v-model="withAuxiliaryColumns">Ajouter les colonnes auxiliaires</b-checkbox>
+    </b-row>
+<!--    <b-row>-->
+      <div v-if="generating"  class="d-flex justify-content-center">
+        <b-spinner label="chargement"  class="m-2"/>
+        <p class="m-2">{{ message }}</p>
+      </div>
+      <div v-else>
+        <div v-if="linkToDownload.length > 0" class="d-flex justify-content-center">
+          <b-button-group>
+            <b-button @click="download" class="m-2">Télécharger le document</b-button>
+            <b-button @click="cancelDownload">Annuler</b-button>
+          </b-button-group>
+        </div>
+      </div>
+<!--    </b-row>-->
 
 
   </b-container>
@@ -21,19 +43,53 @@ export default {
   name: "Export",
   data: function() {
     return {
-
+      generating: false,
+      message: "",
+      linkToDownload: "",
+      withWeed: false,
+      withAuxiliaryColumns: true
     }
   },
   methods: {
-    exporterExcel: function() {
-      axios.get("/export/csv/", ).then(response => {
-        const blob = new Blob([response.data], { type: 'text/csv' })
-        const link = document.createElement('a')
-        link.href = URL.createObjectURL(blob)
-        link.download = `catalogue-${Date.now()}.csv`
-        link.click()
-        URL.revokeObjectURL(link.href)
+    exporterCSV: function() {
+      this.generating = true;
+      this.message = "Génération du fichier CSV en cours...";
+      axios.get(`/export/csv/${params}`).then(response => {
+        if(response.data.success) {
+          this.linkToDownload = response.data.filename;
+        } else {
+          this.message = "Erreur lors de la génération du fichier CSV.";
+        }
+        // const blob = new Blob([response.data], { type: 'text/csv' });
+        // const link = document.createElement('a');
+        // link.href = URL.createObjectURL(blob);
+        // link.download = `catalogue-${Date.now()}.csv`;
+        // link.click();
+        // URL.revokeObjectURL(link.href);
+        this.generating = false;
+        this.message = "";
       })
+    },
+    exporterExcel: function() {
+      this.generating = true;
+      this.message = "Génération du fichier Excel en cours...";
+      // setTimeout(() => {
+      //   this.downloading = false;
+      // }, 10000);
+      axios.get(`/export/xlsx/${params}`, ).then(response => {
+        if(response.data.success) {
+          this.linkToDownload = response.data.filename;
+        } else {
+          this.message = "Erreur lors de la génération du fichier Excel."
+        }
+        // const blob = new Blob([response.data], { type: 'application/csv' }); // vnd.openxmlformats-officedocument.spreadsheetml.sheet
+        // const link = document.createElement('a');
+        // link.href = URL.createObjectURL(blob);
+        // link.download = `catalogue-${Date.now()}.xlsx`;
+        // link.click();
+        // URL.revokeObjectURL(link.href);
+        this.generating = false;
+      });
     },
     exporterPDF: function() {
       // const doc = new jsPDF();
@@ -41,11 +97,30 @@ export default {
       // doc.save("catalogue.pdf");
 
 
+    },
+
+    download: function() {
+      let linkToDownload = this.linkToDownload;
+      window.open(`${process.env.VUE_APP_SITE_API_URL}/static/${linkToDownload}`, '_blank');
+    },
+    cancelDownload: function() {
+      this.linkToDownload = '';
+    }
+  },
+  computed: {
+    params: function() {
+      let params = [];
+      this.withWeed ? params.push("withWeed=1") : params.push("withWeed=0");
+      this.withAuxiliaryColumns ? params.push("withAuxiliaryColumns=1") : params.push("withAuxiliaryColumns=0");
+      return "?"+params.join("&");
     }
   }
 }
 </script>
 
 <style scoped>
+.my-center {
+  position: fixed; top: 50%; left: 50%;
+}
 
 </style>
