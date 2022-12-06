b62bd2cfe71ecee1dd743f8fdc180ad592333d00
Clément Besnier <clemsciences@aol.com>
2022-12-06 23:11:58 +0100

    Added sat_biblio_referencement and search routes

---
+++ sat_biblio_server/routes/search_routes.py
@@ -0,0 +1,21 @@
+from sqlalchemy.orm import Session
+
+from sat_biblio_server.routes import *
+from sat_biblio_referencement import TABLE_PUBLICATION_2004_FILENAME
+from sat_biblio_referencement.database.database_manager import DatabaseManager
+from sat_biblio_referencement.data.search_manager import SearchManager
+from sat_biblio_referencement.scripts.populate_table_publication_2004 import generate_publication_table_2004
+
+TABLE_PUBLICATION_2004_PATH = os.path.join(PACKDIR, "static", TABLE_PUBLICATION_2004_FILENAME)
+sbr_dbm = DatabaseManager(False, False, path=TABLE_PUBLICATION_2004_PATH)
+sbr_dbm.prepare()
+
+
+@sat_biblio.route("/search/works/", methods=["GET"])
+def search_works_route():
+    # is_approximate = request.args.get("approximate", "false") == "true"
+    query = request.args.get("query")
+    session = Session(sbr_dbm.engine)
+    suggestions = SearchManager.get_data_from_query(session, query)
+    session.close()
+    return json_result(True, suggestions=suggestions), 200
