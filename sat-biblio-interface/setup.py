2b93904a35def4ef55264584b5d862a71d3801ec
Clément Besnier <clem@clementbesnier.fr>
2023-10-21 23:14:37 +0200

    Added setup.py

---
+++ sat-biblio-interface/setup.py
@@ -0,0 +1,30 @@
+import os
+import io
+from setuptools import find_packages, setup
+
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+
+with io.open(os.path.join(CUR_DIR, "README.md"), "r", encoding="utf-8") as f:
+    README = f.read()
+
+with io.open(os.path.join(CUR_DIR, "LICENSE"), "r", encoding="utf-8") as f:
+    LICENSE = f.read()
+
+setup(
+    name='sat-biblio-web',
+    version='0.4.0',
+    author='Clément Besnier',
+    author_email='clem@clementbesnier.fr',
+    description='Web app to manage library books.',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url='https://github.com/clemsciences/sat-biblio-flask',
+    packages=find_packages(),
+    include_package_data=True,
+    keywords=['library', 'book', 'MARC', 'UNIMARC', 'INTERMARC'],
+    scripts=[],
+    zip_safe=True,
+    python_requires=">=3.7",
+    license=LICENSE,
+)
+
