e23d541ee2123cd084808547c6dd28fd66c912c4
Clément Besnier <clemsciences@aol.com>
2024-04-22 22:55:32 +0200

    Fix #38

---
+++ sat_biblio_server/scripts/script_generate_publication_table_2004.py
@@ -0,0 +1,9 @@
+"""
+Geneare
+"""
+
+
+from sat_biblio_referencement.scripts.populate_table_publication_2004 import generate_publication_table_2004, CUSTOM_PATH
+
+
+generate_publication_table_2004(CUSTOM_PATH)
